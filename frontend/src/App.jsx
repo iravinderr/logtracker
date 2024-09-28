59a449a22937009eec3ef933154975a567525e4f
Gurnoor Singh <gurnoor1523@gmail.com>
2024-09-28 16:20:28 +0530

    Graph

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Component from "./assets/graph-comp"
+import { ThemeProvider } from "./components/ui/theme-provider"
 
 function App() {
   return (
-    <div>App</div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    {<div><Component/></div>}
+  </ThemeProvider>
+    
   )
 }
 
 export default App
+
+
